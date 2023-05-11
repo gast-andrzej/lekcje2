23f5ac6075cdf7f82390cdeb4739b6addd47f2b7
gast-andrzej <71961055+gast-andrzej@users.noreply.github.com>
2023-05-11 21:22:12 +0200

    Create lambdy_lekcja.py

---
+++ lambdy_lekcja.py
@@ -0,0 +1,28 @@
+
+#
+# n = lambda x=int(input()),y=int(input()) : x+y
+# z = lambda : n()+8
+# g = lambda : print(z()+2)
+# g()
+#
+#
+#
+#
+
+
+
+
+
+# # g = lambda z = input(): print(list(ord(i) for i in z))
+# # g()
+#
+#
+# a = 82
+# print(bin(a))
+
+# toBin = lambda z=input() : print(list(bin(ord(i)) for i in z))
+# toBin()
+
+
+# func1 = lambda : print(list(__import__("random").choice(list(i for i in range(0,100,2))) for i in range(100)))
+# func1()
